9908d3af059d2addbdf09547a168a0d722b5ea39
Cele30 <celkoski.kristijan@uklo.edu.mk>
2021-09-21 20:30:33 +0200

    first commit

--- src/app/modules/admin/projectx2/project2.component.module.ts
+++ src/app/modules/admin/projectx2/project2.component.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { Project2Component } from 'app/modules/admin/projectx2/project2.component';
 import {MatTabsModule} from '@angular/material/tabs';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 const exampleRoutes: Route[] = [
@@ -18,7 +19,9 @@ const exampleRoutes: Route[] = [
     ],
     imports: [
         RouterModule.forChild(exampleRoutes),
-        MatTabsModule
+        MatTabsModule,
+        MatSelectModule,
+        MatDividerModule
     ]
 })
 export class Project2Module {
